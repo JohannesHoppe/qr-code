20d1d31e27be44262db1a126e884140c13c6932a
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-12-07 08:49:17 +0100

    + detecting barcodes from the webcam

--- inc/code.js
+++ inc/code.js
@@ -1,27 +1,72 @@
+let input, detectButton, container;
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  input = document.getElementById('text');
+  container = document.getElementById('container');
+  detectButton = document.getElementById('detect');
+
   refreshQr();
-  const input = document.getElementById('text');
   input.focus();
-  input.select()
 
   input.addEventListener('input', refreshQr);
-
+  detectButton.addEventListener('click', detect);
 });
 
 function refreshQr() {
 
-  const container = document.getElementById('container');
-  const text = document.getElementById('text').value;
+  container.innerHTML = '';
 
-  const qr = QRCode.generateSVG(text, {
-    ecclevel: "M",
-    fillcolor: "white",
-    textcolor: "black",
-    margin: 4,
-    modulesize: 8
-  });
+  const text = input.value;
+  if (text) {
+    const qr = QRCode.generateSVG(text, {
+      ecclevel: 'M',
+      fillcolor: 'white',
+      textcolor: 'black',
+      margin: 4,
+      modulesize: 8
+    });
+
+    container.appendChild(qr);
+  }
+}
+
+// see: https://daily-dev-tips.com/posts/detecting-barcodes-from-the-webcam/
+async function detect() {
 
   container.innerHTML = '';
-  container.appendChild(qr);
+
+  const barcodeDetector = new BarcodeDetector();
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: 'environment' }
+  });
+
+  const video = document.createElement('video');
+  video.srcObject = mediaStream;
+  video.autoplay = true;
+
+  container.appendChild(video);
+  let stop = false;
+
+  function render() {
+
+    barcodeDetector
+      .detect(video)
+      .then((barcodes) => {
+        barcodes.forEach((barcode) => {
+          input.value = barcode.rawValue;
+          stop = true;
+        });
+      })
+      // .catch(console.error);
+  }
+
+  (function renderLoop() {
+    if (stop) {
+      container.innerHTML = '';
+      return;
+    }
+    requestAnimationFrame(renderLoop);
+    render();
+  })();
 }
